98c760990fb00e589f8ef4bcf4d32a9c75bd01ea
Florian-Cocca <90192318+Florian-Cocca@users.noreply.github.com>
2025-01-30 10:06:15 +0100

    L'API ANNONCES MARCHE LESTFUCKINGGOOOOOO

--- backend/setupDB/rechercheDB.mjs
+++ backend/setupDB/rechercheDB.mjs
@@ -1,38 +1,46 @@
-import {cli} from "./connectDB.mjs"
-export { /*met le nom des fonctions ici*/ };
+import {client, getterUser} from "./connectDB.mjs"
+export { addNewPostInDB, getAllPostsInDB, getPostInDB, getReducedInfos, deletePost, updateCityAndArea, updatePost };
 
 //La ville et le rayon de dispo sont pris depuis le profil de l'utilisateur
 //L'id de l'utilisteur est passé par le front avec la variable de session
 //Pour l'instant c'est mit au dur le temps qu'on fasse le back
 async function addNewPostInDB(idUser, isTeacher, subject, meetingPoint, complement, startDate, timetable, onlineMeeting, irlMeeting, canMove){
-
+    //Envoie une annonce dans la DB
     try{
-        const db = cli.db("posts");
-        const collection = db.collection("poststest");
+        //Connexion à la DB
+        const db = client.db("posts");
+        const collection = db.collection("posts");
+        //On envoie l'annonce
         let idPost = 0;
         const lastPost = await collection.find({}).sort({PostID:-1}).limit(1).toArray();
         if(lastPost.length>0){
             idPost=lastPost[0].PostID+1;
         }
-        const result = await collection.insertOne({PostID: idPost, UserID: idUser, IsTeacher: isTeacher, Subject: subject, MeetingPoint: meetingPoint, Complement: complement, AreaSize: 10, City: "Roubaix", StartDate: startDate, Timetable: timetable, OnlineMeeting: onlineMeeting, IRLMeeting: irlMeeting, CanMove: canMove});
-        console.log("Nouveau post banger de Plorian Poppa: ", result); 
+
+        //Recup ville et rayon depuis le profil utilisateur
+        const city = await getterUser("city", idUser);
+        const area = await getterUser("radiusMove", idUser);
+        
+        const result = await collection.insertOne({PostID: idPost, UserID: idUser, IsTeacher: isTeacher, Subject: subject, MeetingPoint: meetingPoint, Complement: complement, AreaSize: area, City: city, StartDate: startDate, Timetable: timetable, OnlineMeeting: onlineMeeting, IRLMeeting: irlMeeting, CanMove: canMove});
+        
+        if(result){
+            console.log("Annonce publiée avec succès");
+            return 1
+        } else{
+            console.log("Erreur de publication");
+            return -1
+        }
     } catch (err){
         console.error("Erreur lors de la mise en ligne de l'annonce: ", err);
         throw err; //Remonte l'erreur
-    }
-
-    
-
-    //Recup ville et rayon, en attendant c'est mit en dur donc normal que ça soit Roubaix et 10km à chaque fois
-
-    
+    }    
 }
 
 async function getAllPostsInDB(){
     //Récupère toutes les annonces dans la DB
     try{
         //Connexion à la DB
-        const db = cli.db("posts");
+        const db = client.db("posts");
         const collection = db.collection("posts");
 
         //Récupération de toutes les annonces
@@ -56,7 +64,7 @@ async function getPostInDB(idPost){
     //Récupère l'annonce dont l'id est idPost
     try{
         //Connexion à la DB
-        const db = cli.db("posts");
+        const db = client.db("posts");
         const collection = db.collection("posts");
 
         //Récupération de l'annonce
@@ -81,18 +89,18 @@ async function getReducedInfos(idUser){
 
     try {
         //Connexion à la DB
-        const db = cli.db("posts");
+        const db = client.db("posts");
         const collection = db.collection("posts");
 
         //On récupère les annonces de l'utilisateur
         const result = await collection.find({UserID:idUser}, {projection: {Subject: 1, PostID: 1, IsTeacher: 1}}).toArray();
 
         if(result){
-            console.log("Annonces récupérées");
+            console.log("Annonces récupérées"); //Retourne array vide si aucune annonce
             return result;
         }
         else{
-            console.log("Aucune annonce trouvées");
+            console.log("Problème lors de la récupération"); //Ne devrait jamais arriver
             return [] //On return un array vide
         }
     
@@ -104,10 +112,9 @@ async function getReducedInfos(idUser){
 
 async function deletePost(idPost){
     //Supprime l'annonce qui a l'id idPost
-
     try{
         //Connexion à la DB
-        const db = cli.db("posts");
+        const db = client.db("posts");
         const collection = db.collection("posts");
 
         //On supprime l'annonce idPost
@@ -115,11 +122,11 @@ async function deletePost(idPost){
 
         //On vérifie si ça a marché
         if (result.deletedCount > 0) {
-            console.log("L'annonce avec l'ID ${idPost} a été supprimée");
+            console.log("L'annonce a été supprimée");
             return 1;
         } else {
-            console.log("Aucune annonce trouvée avec l'ID ${idPost}");
-            return 0;
+            console.log("Aucune annonce trouvée");
+            return -1;
         }
     } catch (err){
         console.error("Erreur lors de la suppression de l'annonce: ", err);
@@ -129,10 +136,9 @@ async function deletePost(idPost){
 
 async function updateCityAndArea(idUser, city, area){
     //Met à jour la ville et le rayon de déplacement de toutes les annonces de l'utilisateur
-
     try {
         //Connexion
-        const db = cli.db("posts");
+        const db = client.db("posts");
         const collection = db.collection("posts");
 
         //On met à jour les annonces de l'user
@@ -141,37 +147,37 @@ async function updateCityAndArea(idUser, city, area){
 
         if (result.modifiedCount > 0) {
             console.log("Les annonces de l'utilisateur ont été mis à jour.");
+            return 1
         } else {
             console.log("Aucune annonce trouvée ou aucune modification nécessaire.");
+            return -1
         }
 
     } catch (err){
         console.error("Erreur lors de la mise à jour des annonces: ", err);
         throw err; //Remonte l'erreur
     }
-
-    
-    
 }
 
 async function updatePost(idPost, isTeacher, subject, meetingPoint, complement, startDate, timetable, onlineMeeting, irlMeeting, canMove){
-
+    //Met à jour une annonce
     try{
-        const db = cli.db("posts");
+        //Connexion à la DB
+        const db = client.db("posts");
         const collection = db.collection("posts");
 
-        
+        //On met à jour l'annonce
+        const updatedFields = {IsTeacher:isTeacher, Subject:subject, MeetingPoint:meetingPoint, Complement:complement, StartDate:startDate, Timetable:timetable, OnlineMeeting:onlineMeeting, IRLMeeting:irlMeeting, CanMove:canMove};
         const result = await collection.updateOne({PostID: idPost}, {$set : updatedFields});
         if (result.modifiedCount > 0) {
-            console.log(`Le post avec l'ID ${idPost} a été mis à jour.`);
+            console.log("L'annonce à été mise à jour");
+            return 1
         } else {
-            console.log(`Aucun post trouvé ou aucune modification nécessaire.`);
+            console.log("Aucune annonce trouvée ou aucune modification nécessaire");
+            return -1
         }
     } catch (err){
         console.error("Erreur lors de la mise à jour de l'annonce: ", err);
         throw err;
     }
-
-    const updatedFields = {IsTeacher:isTeacher, Subject:subject, MeetingPoint:meetingPoint, Complement:complement, StartDate:startDate, Timetable:timetable, OnlineMeeting:onlineMeeting, IRLMeeting:irlMeeting, CanMove:canMove};
-    
 }
